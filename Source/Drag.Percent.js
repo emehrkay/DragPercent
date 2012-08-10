8d43cc53193f7db916df421439cf5e43507d0ddf
Mark Henderson <emehrkay@gmail.com>
2012-08-09 21:20:14 -0400

    updated drag to consider ratio

--- Source/Drag.Percent.js
+++ Source/Drag.Percent.js
@@ -28,6 +28,10 @@ Drag.Percent = new Class({
         this.parent(element, options);
         this.defineScale();
         
+        this.addEvent('onBeforeStart', function(){
+            this.element_position = this.element.getPosition(this.container);
+        }, this);
+
         if(this.options.auto_resize){
             this.windowResize();
         }
@@ -46,13 +50,69 @@ Drag.Percent = new Class({
     defineScale: function(){
         this.container_size = this.container.getSize();
         this.element_size = this.element.getSize();
+        this.element_position = this.element.getPosition(this.container);
         this.percent = {
             x: (this.element_size.x / this.container_size.x),
             y: (this.element_size.y / this.container_size.y) 
         };
-        
+
         return this;
     },
+    
+    start: function(event){
+		var options = this.options;
+
+		if (event.rightClick) return;
+
+		if (options.preventDefault) event.preventDefault();
+		if (options.stopPropagation) event.stopPropagation();
+		this.mouse.start = event.page;
+
+		this.fireEvent('beforeStart', this.element);
+
+		var limit = options.limit;
+		this.limit = {x: [], y: []};
+
+		var z, coordinates;
+		for (z in options.modifiers){
+			if (!options.modifiers[z]) continue;
+
+			var style = this.element.getStyle(options.modifiers[z]);
+
+			// Some browsers (IE and Opera) don't always return pixels.
+			if (style && !style.match(/px$/)){
+				if (!coordinates) coordinates = this.element.getCoordinates(this.element.getOffsetParent());
+				style = coordinates[options.modifiers[z]];
+			}
+
+			if (options.style) this.value.now[z] = (style || 0).toInt();
+			else this.value.now[z] = this.element[options.modifiers[z]];
+
+			if (options.invert) this.value.now[z] *= -1;
+
+			this.mouse.pos[z] = event.page[z];
+
+			if (limit && limit[z]){
+				var i = 2;
+				while (i--){
+					var limitZI = limit[z][i];
+					if (limitZI || limitZI === 0) this.limit[z][i] = (typeof limitZI == 'function') ? limitZI() : limitZI;
+				}
+			}
+		}
+
+		if (typeOf(this.options.grid) == 'number') this.options.grid = {
+			x: this.options.grid,
+			y: this.options.grid
+		};
+
+		var events = {
+			mousemove: this.bound.check,
+			mouseup: this.bound.cancel
+		};
+		events[this.selection] = this.bound.eventStop;
+		this.document.addEvents(events);
+	},
     
     drag: function(event){
         var options = this.options;
@@ -62,7 +122,9 @@ Drag.Percent = new Class({
 
 		for (var z in options.modifiers){
 			if (!options.modifiers[z]) continue;
-			this.value.now[z] = (this.mouse.now[z] - this.mouse.pos[z]) * this.percent[z];
+			
+            var delta_value = (this.mouse.now[z] - this.mouse.pos[z] + this.element_position[z]) * this.percent[z];
+            this.value.now[z] = (delta_value / this.element_size[z]) * 100;
 
 			if (options.invert) this.value.now[z] *= -1;
 
@@ -81,6 +143,11 @@ Drag.Percent = new Class({
 		}
 
 		this.fireEvent('drag', [this.element, event]);
+    },
+    
+    stop: function(event){
+        this.element_position = this.element.getPosition(this.container);
+        this.parent(event);
     }
 });
 
